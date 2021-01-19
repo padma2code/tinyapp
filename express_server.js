6b5e19eabccfb32a7310a740c033b33ada9106f3
Padma2Code <padmavalli.aluri@gmail.com>
2021-01-19 01:07:36 +0000

    updated express_server.js

--- express_server.js
+++ express_server.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/hello", (req, res) => {
+  const templateVars = { greeting: 'Hello World!' };
+  res.render("hello_world", templateVars);
+});
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -13,18 +24,10 @@ app.get("/", (req, res) => {
 app.get("/urls.json", (req, res) => {
     res.json(urlDatabase);
   });
-  app.get("/hello", (req, res) => {
-    res.send("<html><body>Hello <b>World</b></body></html>\n");
-  });
-  app.get("/set", (req, res) => {
-    const a = 1;
-    res.send(`a = ${a}`);
-   });
-   
-   app.get("/fetch", (req, res) => {
-    res.send(`a = ${a}`);
-   });
-
+  //app.get("/hello", (req, res) => {
+    //res.send("<html><body>Hello <b>World</b></body></html>\n");
+  //});
+  
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
