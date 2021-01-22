1e79117b8a74bbe0f0bffd6adf48ebe1222dedda
Padma2Code <padmavalli.aluri@gmail.com>
2021-01-22 02:42:45 +0000

    Added bcrpyt to hash the passwords and compare the passwords

--- helper.js
+++ helper.js
@@ -1,36 +1,42 @@
+const bcrypt = require('bcrypt');
+
 //function to generate random string for short url
 const generateRandomString = () => {
-    return Math.random().toString(36).slice(2,8);
-  };
-  
-  //function to validate email and password
-  const validateEmailPassword = function(email,password, database){
-    for(let data in database){
-      if(database[data]['email'] === email && database[data]['password'] === password){
+  return Math.random().toString(36).slice(2,8);
+};
+
+
+//function to get user id
+const getUserByEmail = function(email, database){
+  for(let data in database){
+    if(database[data]['email'] === email){
+      return database[data];
+    }
+  }
+};
+
+//function to validate email and password
+const validateEmailPassword = function(email,password, database){
+  for(let data in database){
+    if(database[data]['email'] === email){
+      if(bcrypt.compareSync(password,database[data]['password'])){
         return true; 
       }
+      return false;
     }
-    return false;
-  };
-  
-  //function to get user id
-  const getUserId = function(email, database){
-    for(let data in database){
-      if(database[data]['email'] === email){
-        return database[data];
-      }
+  }
+  return false;
+};
+
+//function to get urls based on userID
+const urlsForUser = function(id, database) {
+  let userUrls = {};
+  for (const shortURL in database) {
+    if (database[shortURL]['userID'] === id) {
+      userUrls[shortURL] = database[shortURL];
     }
-  };
-  
-  //function to get urls based on userID
-  const urlsForUser = function(id, database) {
-    let userUrls = {};
-    for (const shortURL in database) {
-      if (database[shortURL]['userID'] === id) {
-        userUrls[shortURL] = database[shortURL];
-      }
-    }
-    return userUrls;
-  };
-  
-  module.exports = { generateRandomString,validateEmailPassword, getUserId, urlsForUser};
+  }
+  return userUrls;
+};
+
+module.exports = { generateRandomString, getUserByEmail, validateEmailPassword,urlsForUser};
